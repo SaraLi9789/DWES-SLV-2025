aaae3124b1807935185ec473de3f49899f1897ab
Sara <120166739+SaraLi9789@users.noreply.github.com>
2025-11-17 19:28:17 +0100

    Add files via upload

---
+++ D3-UD3-Carrito-De-La-Compra/src/main/java/goya/daw2/D3Ud3CarritoDeLaCompraApplication.java
@@ -0,0 +1,13 @@
+package goya.daw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3Ud3CarritoDeLaCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D3Ud3CarritoDeLaCompraApplication.class, args);
+	}
+
+}
